2046c6be78eaa3a4460c4c58bf5aa1bd084dbe77
ilya_popov <il_pop@mail.ru>
2024-07-21 17:03:06 +0300

    получаем данные на текущее впемя

--- meteo/app/get_meteo.py
+++ meteo/app/get_meteo.py
@@ -2,6 +2,7 @@
 - получаем данные о погоде через данные о широте и долготе
 """
 import requests
+import datetime
 
 from . import models
 from . import get_coordinats
@@ -13,45 +14,48 @@ class Meteo:
     def __init__(self, city: str):
         self.city = city
 
-    @staticmethod
-    def __conversion_to_number(number):  # TODO
-        return number
-
     def __get_coordinates(self):
         """
         :return: словарь с координатами долготы и широты типа float
         """
         coordinates = models.City.objects.filter(city=self.city)
-        if coordinates:
-            latitud = self.__conversion_to_number(number=coordinates[0].latitude)  # широта
-            longitude = self.__conversion_to_number(number=coordinates[0].longitude)  # долгота
+        if coordinates:  # если координаты города есть в БД
+            latitude = coordinates[0].latitude  # широта
+            longitude = coordinates[0].longitude  # долгота
+            return {
+                "latitude": latitude,
+                "longitude": longitude,
+            }
         else:  # если в БД нет города
             try:
-                latitude, longitude = get_coordinats.Coordinates(city=self.city)()
+                latitude, longitude = get_coordinats.Coordinates(city=self.city)()  # получаем новые координаты города
                 models.City.objects.create(
                     city=self.city.lower(), latitude=latitude, longitude=longitude)
                 return {
-                    "latitud": latitude,
+                    "latitude": latitude,
                     "longitude": longitude,
                 }
             except:
                 return False
-        return {
-            "latitud": latitud,
-            "longitude": longitude,
-        }
 
-    def __call__(self, *args, **kwargs):
+    def __get_temperature(self):
         coordinates = self.__get_coordinates()
         if not coordinates:
             return False
         params = {
-            "latitude": coordinates.get("latitud"),
+            "latitude": coordinates.get("latitude"),
             "longitude": coordinates.get("longitude"),
             "hourly": "temperature_2m",
         }
         response = requests.post(self.url, data=params).json()
-        temperature_now = response.get("hourly").get("temperature_2m")[0]
+        times = response.get("hourly").get("time")
+        temperatures = response.get("hourly").get("temperature_2m")
+        temperature_dict = dict(zip(times, temperatures))
+        return temperature_dict
+
+    def __call__(self):
+        date_time_now = datetime.datetime.now().strftime('%Y-%m-%dT%H:00')  # дата и время на сейчас
+        temperature_now = self.__get_temperature().get(date_time_now)
         return temperature_now
 
 
